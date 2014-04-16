116f991115bd6b3b892c4f93acf7e397f8b008e2
Kevin Xu <jxu@uoregon.edu>
2014-04-16 09:20:40 -0700

    fixed includes

--- testRawWrite.C
+++ testRawWrite.C
@@ -3,8 +3,12 @@
 using std::cout;
 using std::cerr;
 using std::endl;
+#include <regex>
+using std::regex;
 #include <string>
 using std::string;
+
+#include <ctime>
 
 #include <boost/interprocess/file_mapping.hpp>
 #include <boost/interprocess/mapped_region.hpp>
@@ -13,6 +17,71 @@ namespace bi = boost::interprocess;
 #include "newpack.h"
 using namespace pip;
 #include "fastq.h"
+
+void reportSpeed(unsigned long long n, unsigned long long bytes, clock_t time)
+{
+	double s = time/(double)CLOCKS_PER_SEC;
+	double MB = bytes / 1024 / 1024;
+	printf("Pip: %llu sequences imported in %4.2f seconds: %6.1f rps %6.1f MB/s\n",n,s,n/s,MB/s);
+}
+
+int tok(char* in)
+{
+	char c;
+	for (int n = 0,c = *(in + n); c != 0; ++n) {
+		if (c == ':' || c == ' ' || c == '\n') {
+			return n;
+		}
+	}
+}
+
+void mm_struct(const string& filename)
+{
+	// create a file mapping from filename
+	bi::file_mapping m_file(filename.c_str(),bi::read_write);
+	// create the mapped region of the entire file
+	bi::mapped_region region(m_file,bi::read_write);
+	region.advise(bi::mapped_region::advice_sequential); // inform the OS of our access pattern
+	auto* addr = (char*)region.get_address();
+	pip::fastq fq;
+	fq.instrument = "HWI-ST0747";
+	fq.run = 277;
+	fq.flowcell = "D1M96ACXX";
+	fq.lane = 6;
+	fq.tile = 1101;
+	fq.x = 1232;
+	fq.y = 2090;
+	fq.pair = 1;
+	fq.filter = 'N';
+	fq.control = 0;
+	fq.index = "";
+	fq.sequence = "GGATAGTACTAGGGTATCTAATCCTGTTTGCTCCCCACGCTTTCGCACCTCAGCGTCAGTATCGAGCCAGTGAGCCGCCTTCGCCACTGGTGTTCCTCCGAATATCTACGAATTTCACTGCTACACGCGGAATTCCATCCCCCTCTACCGT";
+	fq.quality = "ACCCFFDDFH?FHHIGIJJFJJJJJJJJJJJIIJJJGIJJGIJJJIGIJJJIIIIJJJJHHHHHFFFDCEACCDDCDDD@BDDDDBDDDDDDDDDDDDDBBB@BDEEACDDDDDDDEDDCCDDDADBBDDDDDDDDECCBDDDB@9@AA<<";
+	printf("size of fq: %d\n", sizeof(fq));
+	memcpy(addr,&fq,sizeof(fq));
+	region.flush();
+	/*
+@HWI-ST0747:277:D1M96ACXX:6:1101:1232:2090 1:N:0:
+GGATAGTACTAGGGTATCTAATCCTGTTTGCTCCCCACGCTTTCGCACCTCAGCGTCAGTATCGAGCCAGTGAGCCGCCTTCGCCACTGGTGTTCCTCCGAATATCTACGAATTTCACTGCTACACGCGGAATTCCATCCCCCTCTACCGT
++
+ACCCFFDDFH?FHHIGIJJFJJJJJJJJJJJIIJJJGIJJGIJJJIGIJJJIIIIJJJJHHHHHFFFDCEACCDDCDDD@BDDDDBDDDDDDDDDDDDDBBB@BDEEACDDDDDDDEDDCCDDDADBBDDDDDDDDECCBDDDB@9@AA<<
+	struct fastq {
+		std::string instrument;
+		int run;
+		std::string flowcell;
+		int lane;
+		int tile;
+		int x;
+		int y;
+		int pair;
+		char filter;
+		int control;
+		std::string index;
+		std::string sequence;
+		std::string quality;
+	};
+	*/
+}
 
 int mm_read_and_insert(const string& filename, const string& outname)
 {
@@ -57,7 +126,7 @@ int mm_read_and_insert(const string& filename, const string& outname)
 		}
 		//o_region.flush();
 		auto endClock = clock() - startClock;
-		printf("Pip: %llu sequences imported in %4.2f seconds\n",n,endClock/(double)CLOCKS_PER_SEC);
+		reportSpeed(n,size,endClock);
 		return n; // return number of rows inserted
 	}
 	return 0;
@@ -102,7 +171,7 @@ int ins_read_and_insert(const string& filename, const string& outname)
 		}
 		//o_region.flush();
 		auto endClock = clock() - startClock;
-		printf("Pip: %llu sequences imported in %4.2f seconds\n",n,endClock/(double)CLOCKS_PER_SEC);
+		reportSpeed(n,contents.size(),endClock);
 		return n; // return number of rows inserted 
   }
 	return 0;
@@ -111,8 +180,12 @@ int ins_read_and_insert(const string& filename, const string& outname)
 int main (int argc, char const *argv[])
 {
 	if (argc == 2) {
+		/*cout << "mm read and insert\n";
 		mm_read_and_insert(argv[1],"out.test");
-		ins_read_and_insert(argv[1],"out.test");
+		cout << "instream read and insert\n";
+		ins_read_and_insert(argv[1],"out.test");*/
+		cout << "mm write\n";
+		mm_struct("data/cfirst3.fastq");
 		return 0;
 	}
 	cout << "Usage: ./testRawWrite <fastq file>" << endl;
